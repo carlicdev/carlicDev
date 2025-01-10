311a148dea4a876e9cc73f705e245592988b15cf
carlos camacho <carlic.dev@gmail.com>
2025-01-10 06:52:10 -0600

    fixed hero for mobile

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -6,9 +6,9 @@ const Hero = () => {
     <div className='max-w-lg my-14 mx-auto flex flex-col gap-7'>
         <p className='text-logo uppercase font-ligth text-2xl tracking-widest text-center'>Carlic</p>
         <div className='flex flex-col items-center'>
-            <p className='font-black text-6xl tracking-tighter text-center capitalize'>libre.</p>
-            <p className='font-black text-6xl tracking-tighter text-center capitalize'>indomable.</p>
-            <p className='font-black text-6xl tracking-tighter text-center capitalize'>indestructible.</p> 
+            <p className='font-black text-5xl md:text-6xl tracking-tighter text-center capitalize'>libre.</p>
+            <p className='font-black text-5xl md:text-6xl tracking-tighter text-center capitalize'>indomable.</p>
+            <p className='font-black text-5xl md:text-6xl tracking-tighter text-center capitalize'>indestructible.</p> 
         </div>
         <p className='text-lg text-center'>Construye una masculinidad poderosa. Reclama tu libertad. Toma el control de tu vida.</p>
         <p className='text-lg text-center'>Lo único que necesitas para conseguir todo lo que deseas es convertirte en un <span className='font-bold text-orange-700'>Hombre De 5 Herramientas.</span></p>
